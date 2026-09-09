4164edc2a9bb6d0d66b39aa3ad37df193ddfe877
Yakup Murat Çaylak <y.muratcaylak@outlook.com>
2026-09-09 16:03:49 +0300

    feat: Define studio screen UI on MainActivity

--- app/src/main/java/com/caylakym/aberritual/MainActivity.kt
+++ app/src/main/java/com/caylakym/aberritual/MainActivity.kt
@@ -4,44 +4,30 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.viewModels
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import com.caylakym.aberritual.data.repository.WallpaperRepositoryImpl
+import com.caylakym.aberritual.ui.creator.StudioScreen
+import com.caylakym.aberritual.ui.creator.StudioViewModel
 import com.caylakym.aberritual.ui.theme.AberritualTheme
 
 class MainActivity : ComponentActivity() {
+
+    private val viewModel: StudioViewModel by viewModels {
+        StudioViewModel.Factory(WallpaperRepositoryImpl(applicationContext))
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             AberritualTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                Surface(modifier = Modifier.fillMaxSize()) {
+                    StudioScreen(viewModel = viewModel)
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    AberritualTheme {
-        Greeting("Android")
     }
 }
