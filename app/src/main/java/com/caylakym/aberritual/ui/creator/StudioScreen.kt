5c2a468b320ec88ed32ad241dadb1f07aca5758c
Yakup Murat Çaylak <y.muratcaylak@outlook.com>
2026-09-09 20:46:59 +0300

    fix: Add navigation bar padding to the `StudioScreen` to fix the issue of navbar covering the app on older devices

--- app/src/main/java/com/caylakym/aberritual/ui/creator/StudioScreen.kt
+++ app/src/main/java/com/caylakym/aberritual/ui/creator/StudioScreen.kt
@@ -13,6 +13,7 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
@@ -92,7 +93,9 @@ fun StudioScreen(
         bottomBar = {
             Surface(
                 tonalElevation = 6.dp,
-                modifier = Modifier.fillMaxWidth()
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .navigationBarsPadding()
             ) {
                 Row(
                     modifier = Modifier
